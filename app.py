7471e61ce8cb28886af625e048cbb18261ff7f13
K.Bonev-Wagner <kerstin-wagner@gmx.at>
2020-04-11 17:10:40 +0200

    [feat] Flask-Migrate for version control of database status

--- app.py
+++ app.py
@@ -2,22 +2,22 @@ import sys
 
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:301191@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
-
-
-db.create_all()
 
 
 @app.route('/todos/create', methods=['POST'])
