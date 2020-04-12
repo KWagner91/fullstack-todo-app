2e60e0ee9fdd8425d80d161c1a8ad2a8f26f180a
K.Bonev-Wagner <kerstin-wagner@gmx.at>
2020-04-12 15:09:55 +0200

    [feat] tasks can be deleted

--- app.py
+++ app.py
@@ -54,6 +54,17 @@ def set_completed_todo(todo_id):
         db.session.close()
     return redirect(url_for('index'))
 
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({ 'success': True })
+
 @app.route('/')
 def index():
     return render_template('index.html', data=Todo.query.order_by('id').all())
