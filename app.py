1a07e27488f8d7efad1de648fa4044bde95f4f3b
K.Bonev-Wagner <kerstin-wagner@gmx.at>
2020-04-12 11:17:44 +0200

    [feat] checkbox to update status was added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -40,10 +40,23 @@ def create_todo():
     if not error:
         return jsonify(body)
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all()
-                           )
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 
 if __name__ == '__main__':
